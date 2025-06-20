0d860d75048a1713a10e08540f2321abda32c953
kalaiselvipalani02 <kalaiselvi.palani02@gmail.com>
2025-06-20 19:17:17 +0530

    implemented Session

--- myfirst-express/server.js
+++ myfirst-express/server.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const PORT = 3000 || process.env.PORT;
 const URL = process.env.URL;
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 //Instance of express
 const app = express();
 
@@ -45,17 +47,47 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //cookie middleware
 app.use(cookieParser());
-
+//session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60 * 60 * 100, //expires 1 hour
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.URL,
+      collectionName: "sessions",
+    }),
+  })
+);
 //----Custom middleware ---
 
 //!--- isAuthenticated (Authentication)
-
+const isAuthenticated = (req, res, next) => {
+  const username = req.session.userData ? req.session.userData.username : null;
+  if (username) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
 //!-- isAdmin (Authorization)
-
-//!-- configure Express session
-
+const isAdmin = (req, res, next) => {
+  const admin = req.session.userData.role === "admin";
+  if (admin) {
+    return next();
+  } else {
+    res.send({
+      message: "Forbidden Access",
+    });
+  }
+};
 //home route
 app.get("/", (req, res) => {
+  console.log(req.session);
+
   res.json({
     message: "Welcome to APP",
   });
@@ -78,6 +110,12 @@ app.post("/login", async (req, res) => {
     username,
   });
   if (userFound && (await bcrypt.compare(password, userFound.password))) {
+    //create session
+    req.session.userData = {
+      username: userFound.username,
+      role: userFound.role,
+    };
+
     res.json({
       message: "Login successful",
     });
@@ -89,18 +127,18 @@ app.post("/login", async (req, res) => {
 });
 
 //Admin route for Admin user only access
-app.get("/admin-only", (req, res) => {
+app.get("/admin-only", isAdmin, (req, res) => {
   res.json({
     message: "Admin Dashboard",
   });
 });
 //submit details
-app.get("/dashboard", (req, res) => {
+app.get("/dashboard", isAuthenticated, (req, res) => {
   //grab the user from cookie
-  const userData = req.cookies.userData
-    ? JSON.parse(req.cookies.userData)
-    : null;
-  const username = userData ? userData.username : null;
+  //const userData = req.session.userData ? req.session.userData.username : null
+
+  const username = req.session.userData ? req.session.userData.username : null;
+
   //const userData = req.cookies()
   //render the dashboard or login page
   if (username) {
@@ -118,7 +156,7 @@ app.get("/dashboard", (req, res) => {
 //logout
 app.get("/logout", (req, res) => {
   //clear cookie
-  res.clearCookie("userData");
+  req.session.destroy("userData");
   res.json({
     message: "Logout successfully",
   });
